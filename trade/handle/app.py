8f1bccf58797cf605c2280feb4741246d64b6d96
DogRing <changh232@naver.com>
2025-06-02 17:11:26 +0000

    handle multi timeframe

--- trade/handle/app.py
+++ trade/handle/app.py
@@ -1,54 +1,146 @@
-from data_transform import data_transform
+from local_values import data_folder
+from data_transform import data_transform,data_scale
 from ttrade import ttrade
-from kafka import KafkaConsumer
-import pandas as pd
+
+from confluent_kafka import Consumer
 import numpy as np
+import pandas as pd
 import json
+import copy
 import time
+import ast
 import os
 
-topic=os.environ['TOPIC']
-tick=os.environ['TICK']
-column_names=['timestamp','open','high','low','close','value']
-interval=int(60/int(os.environ.get('INTERVAL','15')))
-data_count=int(os.environ.get('DATA_LENGTH','200'))*interval
-scaler_path = os.environ.get('SCALER_PATH','/data/data/scaler/')
-kafka_host=os.environ.get('KAFKA_SERVICE','my-cluster-kafka-bootstrap.kafka.svc:9092')
-tf_config_file=os.environ.get('DATA_CONFIG','/etc/config/transform.json')
-config_file=os.environ.get('trade_config','/etc/config/trade.json')
-with open(tf_config_file,'r') as f:
-    tf_config=json.load(f)
-with open(config_file,'r') as f:
-    config=json.load(f)
-input_count=int(os.environ.get('INPUT_LENGTH',config['seq_len']))
+TICK = os.environ.get('TICK','KRW-BTC')
+TOPIC = os.environ.get('TOPIC','krw-btc')
+INTERVAL = int(os.environ.get('INTERVAL','30'))
+DATA = int(os.environ.get('DATA_LENGTH','10000'))
+LARGE_N = ast.literal_eval(os.environ.get('LARGE','[]'))
+DATA_N = ast.literal_eval(os.environ.get('DATA_N_LENGTH','{}'))
 
-consumer=KafkaConsumer(
-    topic,
-    bootstrap_servers=[kafka_host],
-    auto_offset_reset='earliest',
-    enable_auto_commit=False,
-    group_id='my-group',
-    value_deserializer=lambda x: json.loads(x.decode('utf-8')),
-    consumer_timeout_ms=1000
-)
+DATA_CONFIG = os.environ.get('DATA_CONFIG',f'{data_folder}indicator.json')
+SCALER_CONFIG = os.environ.get('SCALER_CONFIG',f'{data_folder}scale.json')
+
+cols = ['open','high','low','close','value']
+SEP = 60 // INTERVAL
+ROWS = DATA * SEP
+COLS = len(cols)
+
+with open(DATA_CONFIG,'r') as f:
+    tf_config=json.load(f)
+with open(SCALER_CONFIG,'r') as f:
+    sc_config=json.load(f)
+
+n_config = copy.deepcopy(tf_config)
+n_config["data"] = [i for i in tf_config["data"] if "RANGE" in i and i["RANGE"] in LARGE_N]
+tf_config["data"] = [i for i in tf_config["data"] if "RANGE" not in i or i["RANGE"] not in LARGE_N]
+
+consumer = Consumer({
+    'bootstrap.servers' : 'my-cluster-kafka-bootstrap.kafka.svc.cluster.local:9092',
+    'group.id' : f'handle-{TOPIC}',
+    'auto.offset.reset' : 'earliest',
+    'enable.auto.commit' : False,
+})
+consumer.subscribe([TOPIC]+[f'{TOPIC}-{n}' for n in LARGE_N])
+
+
+x_buf = np.zeros((ROWS, COLS), dtype=np.float32)
+t_buf = np.zeros(ROWS, dtype=np.float32)
+index_ns = np.zeros(ROWS, dtype=np.int64)
+base_df = pd.DataFrame(x_buf, columns=cols, index=pd.DatetimeIndex(index_ns))
+time_df = pd.DataFrame(t_buf, columns=['timestamp'], index=pd.DatetimeIndex(index_ns))
+order_tem = np.arange(ROWS, dtype=np.int64)
+x_idx=np.where(order_tem % SEP == (SEP-1))[0]
+
+n_buf = { k: np.zeros((v,COLS), dtype=np.float32) for k,v in DATA_N.items() }
+n_t_buf = { k: np.zeros(v, dtype=np.float32) for k,v in DATA_N.items() }
+n_base_df = {k: pd.DataFrame(n_buf[k],  columns=cols) for k in DATA_N }
+n_time_df = {k: pd.DataFrame(n_t_buf[k], columns=['timestamp']) for k in DATA_N }
+_n_order = {k: np.arange(v) for k,v in DATA_N.items() } 
+n_order = _n_order
+n_head = { k: 0 for k in LARGE_N }
+head = 0
+_index_values = base_df.index.view("int64")
+
+def angle_encoding(timestamp):
+    weights = { 'hour':0.1,'day':0.4,'week':0.3,'year':0.2 }
+    
+    hour_angle = (timestamp%3600) / 3600 * 2 * np.pi
+    day_angle = (timestamp%86400) / 86400 * 2 * np.pi
+    week_angle = (timestamp%604800) / 604800 * 2 * np.pi
+    year_seconds = 365.25 * 86400
+    year_angle = (timestamp % year_seconds)/year_seconds*2*np.pi
+    
+    sin_sum = (
+        weights['hour']*np.sin(hour_angle)+
+        weights['day']*np.sin(day_angle)+
+        weights['week']*np.sin(week_angle)+
+        weights['year']*np.sin(year_angle)
+    )
+    cos_sum = (
+        weights['hour']*np.cos(hour_angle)+
+        weights['day']*np.cos(day_angle)+
+        weights['week']*np.cos(week_angle)+
+        weights['year']*np.cos(year_angle)
+    )
+    final_angle = (np.arctan2(sin_sum,cos_sum)+np.pi)/(2*np.pi)
+    return final_angle
+
+def feed_one(m):
+    global head
+    val = json.loads(m.value().decode('utf-8'))
+    timestamp = int(val['timestamp'] * 1e9)
+
+    parts = m.topic().split('-')
+    if len(parts)== 2:
+        x_buf[head] = [val[k] for k in cols]
+        t_buf[head] = angle_encoding(timestamp)
+        _index_values[head] = timestamp
+        head = (head + 1) % ROWS
+        return False
+    elif len(parts)== 3:
+        l_n = parts[2]
+        n_buf[l_n][n_head[l_n]] = [val[k] for k in cols]
+        n_t_buf[l_n][n_head[l_n]] = angle_encoding(timestamp)
+        n_head[l_n] = (n_head[l_n] + 1) % DATA_N[l_n]
+        return l_n
+    else:
+        print("error in topic value")
 
 try:
-    num_features=len(column_names)
-    x_data=np.zeros((data_count,num_features),dtype=np.float64)
-    data_index=np.where(np.arange(data_count) % interval == (interval-1))[0]
-    while data_count > 0:
-        for message in consumer:
-            x_data=np.roll(x_data,-1,axis=0)
-            for i in range(num_features):
-                x_data[-1][i]=message.value[column_names[i]]
-            data_count-=1
-    print(f"timestamp: {time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(message.value['timestamp']))}\ntick: {message.value['tick']}\noffset: {message.offset}\n")
+    batch = consumer.consume(num_messages=ROWS+sum(DATA_N.values()), timeout=-1)
+    for msg in batch:
+        feed_one(msg)
+    
+    msg = consume.poll(timeout=10)
+    while msg is not None:
+        feed_one(msg)
+        msg = consumer.poll(timeout=1)
+
+    print(f"{time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(int(x_buf[-1][0])))}\tFirst batch: {len(batch)}")
+
     while True:
-        for message in consumer:
-            x_data=np.roll(x_data,-1,axis=0)
-            for i in range(num_features):
-                x_data[-1][i]=message.value[column_names[i]]
-            df=data_transform(pd.DataFrame(x_data[data_index,:],columns=column_names),tf_config,path=scaler_path,file_name=tick,save=False)
-            ttrade(df[config['features']][-input_count:].values.reshape(1,-1,len(config['features'])))
+        msg = consumer.poll(timeout=INTERVAL+5)
+        if msg is None:
+            print(f"{time.strftime('%Y-%m-%d %H:%M:%S')}\t No Message in {INTERVAL+5}")
+        elif msg.error():
+            print("Error: ",msg.error())
+        else:
+            n_res = feed_one(msg)
+            if not n_res:
+                order = (order_tem + head) % ROWS
+                x_view = base_df.take(order)
+                dfs = data_transform(x_view,tf_config,0)
+                t_view = { k:time_df.loc[dfs[k].index] for k in dfs }
+
+                n_x_view = {k: n_base_df[k].take(n_order[k]) for k in DATA_N }
+                n_t_view = {k: n_time_df[k].take(n_order[k]) for k in DATA_N }
+                n_dfs = data_transform(n_x_view,n_config,2)
+
+                dfs = data_scale(dfs|n_dfs,sc_config,save=False,path=f'{data_folder}{sc_config.get("path")}/{TICK}/')
+                print(f"{time.strftime('%Y-%m-%d %H:%M:%S')}\t apply ttrade")
+                ttrade(dfs,t_view|n_t_view)
+            else:
+                n_order[n_res] = _n_order[n_res] + n_head[n_res] % DATA_N[n_res]
 finally:
     consumer.close()
--- trade/handle/ttrade.py
+++ trade/handle/ttrade.py
@@ -7,16 +7,40 @@ url=os.environ['MODEL_URL']
 access_key=os.environ['ACCESS_KEY']
 secret_key=os.environ['SECRET_KEY']
 upbit=pyupbit.Upbit(access_key,secret_key)
+MODEL_CONFIG = os.environ.get('MODEL_CONFIG',f'{data_folder}model.json')
 
-def ttrade(x_data):
-    data_bytes = x_data.tobytes()
+with open(MODEL_CONFIG,'r') as f:
+    md_config=json.load(f)
+
+timeframes = md_config.get("timeframes")
+valid_cols = md_config.get("cols")
+in_dims = md_config.get("inputs")
+position = 0
+
+def ttrade(dfs,time_df):
+    global position
+    x_parts = [dfs[tf][valid_cols[tf]].iloc[-in_dims[tf]:]
+            .to_numpy(dtype=np.float32, copy=False)
+            for tf in timeframes]
+    t_parts = [t_view[tf].iloc[-in_dims[tf]:]
+            .to_numpy(dtype=np.float32, copy=False)
+            for tf in timeframes]
+    payload = b''.join(
+        x.tobytes() + t.tobytes()
+        for x, t in zip(x_parts,t_parts)
+    )
+    pos = np.array([position], dtype=np.float32)
+    payload += pos.tobytes()
+
     headers={"Content-Type":"application/octet-stream"}
-    response=requests.post(url,data=data_bytes,headers=headers)
+    response=requests.post(url,data=payload,headers=headers)
     result=np.array(response.json())
-    print(f"last: {x_data[0,-3:,1]}")
-    print(f"prob: {result[:3,0]}")
 
-    # if buy: 
-    #     print(upbit.buy_market_order(ticker, upbit.get_balance("KRW")*0.985))
-    # elif sell:
-    #     print(upbit.sell_market_order(ticker, upbit.get_balance(ticker)*0.985))
+    print(f"{result}")
+
+    if result == 0: 
+        print(upbit.buy_market_order(ticker, upbit.get_balance("KRW")*0.985))
+        postion = 1
+    elif result == 2:
+        print(upbit.sell_market_order(ticker, upbit.get_balance(ticker)*0.985))
+        position = 0
